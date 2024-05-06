d18d57a42b3257c25b829bef82ff494c874dc2d7
Daniel McMahan <64721048+Debug-Dan@users.noreply.github.com>
2024-05-06 18:35:58 -0400

    1

--- modpack-launcher.bat
+++ modpack-launcher.bat
@@ -2,6 +2,8 @@
 title Modpack Launcher Updater
 echo Downloading newest version of launcher....
 echo.
+mkdir "modpack-launcher-files"
+cd modpack-launcher-files
 curl.exe -o part2.bat https://debug-dan.github.io/modpack-updater/part2.bat
 curl.exe -o part3.bat https://debug-dan.github.io/modpack-updater/part3.bat
 start part2.bat
