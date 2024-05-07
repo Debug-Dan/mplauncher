7fd320f90653ab0d4a9c6e090737a942bc8c8f84
Daniel McMahan <64721048+Debug-Dan@users.noreply.github.com>
2024-05-06 20:22:20 -0400

    Update part2.bat

--- part2.bat
+++ part2.bat
@@ -1,5 +1,5 @@
 @echo off
-title Modpack Launcher v1.5
+title Modpack Launcher v1.6
 pushd
 goto Start
 
@@ -202,6 +202,7 @@ cd %appdata%
 cd PrismLauncher
 cd instances
 cd PlasmaticPack
+cd .minecraft
 rmdir /s /q mods
 curl.exe -Lo updateplasmatic.exe "https://drive.usercontent.google.com/download?id=1NzHtV0TxGyHJBc84kYRdl9tW83rFsCKe&export=download&authuser=0&confirm=t&uuid=92e8f110-ae7d-421e-b83b-03d24176c3a9&at=APZUnTUADRIXkssz22JAGi2caEin:1714177258648"
 echo.
