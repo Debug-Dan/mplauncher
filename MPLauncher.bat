1fe5a7b0ed8cd95b8ca5984469770f200b80dccd
Daniel McMahan <64721048+Debug-Dan@users.noreply.github.com>
2024-07-04 16:34:13 -0400

    Update MPLauncher.bat

--- MPLauncher.bat
+++ MPLauncher.bat
@@ -1,6 +1,6 @@
 @echo off
 title MPLauncher Updater
-echo MPLauncher Updater  (U24.7.3)
+echo MPLauncher Updater  (U24.7.4)
 echo
 echo Downloading newest version of launcher....
 echo If no progress is being made, you may need to check your internet connection.
@@ -9,4 +9,4 @@ mkdir mplauncher-files
 cd mplauncher-files
 curl.exe -o part2.bat https://debug-dan.github.io/mplauncher/part2.bat
 curl.exe -o part3.bat https://debug-dan.github.io/mplauncher/part3.bat
-start part2.bat
+start cmd.exe /c part2.bat
